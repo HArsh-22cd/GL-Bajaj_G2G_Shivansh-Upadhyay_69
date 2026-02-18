11a5d52c5db9cf44ce504aab6acbfa3efd5e2661
Shivansh Upadhyay <upadhyayshivansh8394@gmail.com>
2026-02-18 23:43:16 +0530

    Add files via upload

---
+++ equal.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+    int a,b;
+    printf("Enter a = ");
+    scanf("%d",&a);
+    printf("Enter b = ");
+    scanf("%d",&b);
+    if(a==b)
+    printf("numbers are equal");
+    else
+    printf("numbers are not equal");
+    return 0;
+}
